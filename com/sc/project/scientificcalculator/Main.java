7a806d6106c4e10af99a901bbcd9a87b36af708f
Seth Cortel <seth@manila.ammic.com>
2023-12-02 19:24:50 +0800

    Moved MainTest to tester.common

--- com/sc/project/scientificcalculator/Main.java
+++ com/sc/project/scientificcalculator/Main.java
@@ -1,5 +1,5 @@
 package com.sc.project.scientificcalculator;
-import com.sc.project.scientificcalculator.tester.MainTest;
+import com.sc.project.scientificcalculator.tester.common.MainTest;
 
 public class Main {
     public static void main(String[] args) {
