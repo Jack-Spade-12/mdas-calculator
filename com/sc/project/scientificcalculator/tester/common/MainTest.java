57cedc7dc5ba81be70d239dacc8792fd8ac2d4ea
Seth Cortel <seth@manila.ammic.com>
2023-12-02 19:28:53 +0800

    Added tester.common.MainTest

---
+++ com/sc/project/scientificcalculator/tester/common/MainTest.java
@@ -0,0 +1,21 @@
+/**
+ * History
+ * 		
+ * 		December 2, 2023 - S. Cortel - Created
+ * 
+ * Purpose
+ * 		
+ * 		Main tester class to run all tester classes
+ *      
+ */
+package com.sc.project.scientificcalculator.tester.common;
+
+import com.sc.project.scientificcalculator.tester.ValueCheckerTest;
+
+public class MainTest {
+    
+    public MainTest() {
+        // new ValueCheckerTest();
+    }
+
+}
